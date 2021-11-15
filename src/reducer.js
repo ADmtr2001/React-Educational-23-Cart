64ec8eb37d074fd89b762df674465447f7b8699b
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-15 18:31:22 +0200

    add remove item button logic

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,14 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
     if (action.type === 'CLEAR_CART') {
         return { ...state, cart: [], };
+    }
+    if (action.type === 'REMOVE') {
+        return {
+            ...state,
+            cart: state.cart.filter((CartItem) => CartItem.id !== action.payload)
+        }
     }
 
     return state;
