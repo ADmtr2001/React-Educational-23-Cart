3c2ebb09e893294a472d1a5d6ba5bcdde2093384
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-16 15:33:21 +0200

    collapse increase and decrease functions into one toggleAmount function

--- src/reducer.js
+++ src/reducer.js
@@ -56,8 +56,25 @@ const reducer = (state, action) => {
     if (action.type === 'DISPLAY_ITEMS') {
         return { ...state, cart: action.payload, loading: false };
     }
+    if (action.type === 'TOGGLE_AMOUNT') {
+        let tempCart = state.cart
+            .map((cartItem) => {
+                if (cartItem.id === action.payload.id) {
+                    if (action.payload.type === 'inc') {
+                        return { ...cartItem, amount: cartItem.amount + 1 };
+                    }
+                    if (action.payload.type === 'dec') {
+                        return { ...cartItem, amount: cartItem.amount - 1 };
+                    }
+                }
+                return cartItem;
+            })
+            .filter((cartItem) => cartItem.amount !== 0);
 
-    return state;
+        return { ...state, cart: tempCart };
+    }
+
+    throw new Error('no matching action type');
 }
 
 export default reducer;
